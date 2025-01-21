192ccc3783b3274c464d1ee38fb6cb6a2f4eaf4b
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-01-21 11:41:16 +0500

    2104

---
+++ onlineHW/21.01js/main.js
@@ -0,0 +1,55 @@
+// 1
+/*{
+    let age = +prompt("Введите ваш возраст");
+    if (age > 0 && age <= 12) {
+        document.write(`<p>Вы ребенок т.к вам ${age} лет</p>`);
+    } else if (age > 12 && age <= 18) {
+        document.write(`<p>Вы подросток</p>`);
+    } else if(age > 18 && age <= 60) {
+        document.write("<p>Вы взрослый</p>");
+    } else {
+        document.write("<p>Вы пенсионер</p>");
+    }
+}*/
+
+//2
+/*{
+    let sym = +prompt("Введите цифру 0-9")
+    switch(sym) {
+        case 0:
+            document.write(")");
+            break;
+        case 1:
+            document.write("!");
+            break;
+        case 2:
+            document.write("@");
+            break;
+        case 3:
+            document.write("#");
+            break;
+        case 4:
+            document.write("$");
+            break;
+        case 5:
+            document.write("%");
+            break;
+        case 6:
+            document.write("^");
+            break;
+        case 7:
+            document.write("&");
+            break;
+        case 8:
+            document.write("*");
+            break;
+        case 9:
+            document.write("(");
+            break;
+    }
+}*/
+
+//3
+{
+    
+}
