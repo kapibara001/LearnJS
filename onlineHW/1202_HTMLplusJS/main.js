a23d8bbf38e25008983d5cd1b38417f513ab67e7
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-02-07 01:39:15 +0500

    new

--- onlineHW/1202_HTMLplusJS/main.js
+++ onlineHW/1202_HTMLplusJS/main.js
@@ -1,9 +1,33 @@
-{
+{ // 1 
     let fatherElement = document.createElement('div');
     fatherElement.id = 'forParagraph';
+    document.body.appendChild(fatherElement);
+
     let element = document.getElementById('forParagraph');
     let Paragraph = document.createElement('p');
 
-    Paragraph.appendChild("Привет Мир!");
+    Paragraph.innerText = "1) Привет Мир!";
     element.appendChild(Paragraph);
 }
+
+{ // 2, 3, 4
+    let forChange = document.getElementById("myElement");
+    forChange.innerText = "2.1) Новый текст";
+
+    forChange.style.backgroundColor = "blue"; // 3
+
+    forChange.remove(); // 4
+
+    // debugger;
+    // let forChangeClone = 
+
+    let forChangeClass = document.getElementsByClassName("generalClass");
+    for (let i = 0; i < forChangeClass.length; i++) {
+        document.getElementsByClassName("generalClass")[i].innerText = "2.2) Измнененный текст"
+
+        document.getElementsByClassName("generalClass")[i].style.backgroundColor = "red"; // 3
+
+        document.getElementsByClassName("generalClass")[i].remove(); // 4
+    }
+}
+
