cba006ffecc618d6000f45d3cab83d3a647231ae
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-02-07 12:48:56 +0500

    avocado

--- onlineHW/1202_HTMLplusJS/main.js
+++ onlineHW/1202_HTMLplusJS/main.js
@@ -13,21 +13,33 @@
 { // 2, 3, 4
     let forChange = document.getElementById("myElement");
     forChange.innerText = "2.1) Новый текст";
-
+    
     forChange.style.backgroundColor = "blue"; // 3
-
-    forChange.remove(); // 4
-
+    
+    // forChange.remove(); // 4
+    
     // debugger;
     // let forChangeClone = 
-
+    
     let forChangeClass = document.getElementsByClassName("generalClass");
     for (let i = 0; i < forChangeClass.length; i++) {
         document.getElementsByClassName("generalClass")[i].innerText = "2.2) Измнененный текст"
-
+        
         document.getElementsByClassName("generalClass")[i].style.backgroundColor = "red"; // 3
-
-        document.getElementsByClassName("generalClass")[i].remove(); // 4
+        
+        // document.getElementsByClassName("generalClass")[i].remove(); // 4
     }
 }
 
+{ // 5
+    let elem = document.getElementById("myElement");
+
+    // let clone = Object.assign(elem);
+    let clone = elem.cloneNode(true); // копируемныйЭлемент.cloneNode(true/false)
+
+    document.body.appendChild(clone);
+}
+
+{  // 6
+    
+}
