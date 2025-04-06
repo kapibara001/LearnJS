e6d4c2481d73ab1800889db51660db65d3594a01
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-04-06 12:06:24 +0500

    new;

--- onlineHW/react0304/src/assets/components/htmlPage/Page.jsx
+++ onlineHW/react0304/src/assets/components/htmlPage/Page.jsx
@@ -28,23 +28,44 @@ export function Page() {
         )
     }
 
+    function SectionMainContent(props) {
+        const {number, ...any} = props;
+
+        return (
+            <div className={styles['section']}>
+                <p>
+                    <b>Section {number}</b>
+                </p>
+            </div>
+        )
+    }
+
     return (
         <div className={styles['bigPart']}>
             <div className={styles['centerPage']}>
                 <LongModule text={'Header'} color={'purple'} height={'70px'}/>
                 <LongModule text={'Menu'} color={'#C71585'} height={'50px'}/>
-                <div style={{display: 'flex'}}>
+                <div style={{display: 'flex', marginBottom: '10px'}}>
                     <div style={{
-                        width: '100%', 
+                        width: 'max-content', 
                         display: 'flex',
                         flexDirection: 'column',
                     }}>
                         <ShortModule text={'Aside 1'} width={'200px'} height={'90px'}/>
                         <ShortModule text={'Aside 2'} width={'200px'} height={'120px'}/>
                     </div>
-                    <div></div>
-                    <div></div>
+                    <div className={styles['centerModule']}>
+                        <p>
+                            <b>Main Content</b>
+                            <SectionMainContent number={1}/>
+                            <SectionMainContent number={2}/>
+                        </p>
+                    </div>
+                    <div>
+                        <ShortModule text={'Aside 3'} width={'120px'} height={'200px'}/>
+                    </div>
                 </div>
+                <LongModule text={'Footer'} color={'purple'} height={'50px'}/>
             </div>
         </div>
     )
